f01dc89e0085b4a9e75ef66bde5d15400861d12a
ORION PAX <www.shamil.com91@gmail.com>
2025-12-10 14:11:39 +0300

    methods is done

---
+++ src/methods/Main.java
@@ -0,0 +1,34 @@
+package methods;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getFactorial(-723));
+        System.out.println(isPrime(7));
+    }
+
+    public static int getFactorial(int number) {
+        if (number == 0) {
+            return 1;
+        } else if (number < 0) {
+            return -1;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
