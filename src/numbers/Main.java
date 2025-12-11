c0ce2098e3f9446d903acf3ac74b3710f9516fe2
ORION PAX <www.shamil.com91@gmail.com>
2025-12-11 11:52:49 +0300

    numbers is done

---
+++ src/numbers/Main.java
@@ -0,0 +1,33 @@
+package numbers;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(abs(-7));
+        System.out.println(fact(5));
+        System.out.println(safeAvg(10, 8));
+    }
+
+    public static int abs(int number){
+        return number < 0 ? -number : number;
+    }
+
+    public static boolean isPowerOfTwo(int number){
+        if (number <= 0) return false;
+        return (number & (number - 1)) == 0;
+    }
+
+    public static long fact(int number) {
+        long factorial = 1;
+        if(number == 0){
+            return factorial;
+        }
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static int safeAvg(int firstNumber, int secondNumber){
+            return firstNumber + (secondNumber - firstNumber) / 2;
+    }
+}
