f946f2dc4751982d8973087821e285b47abeccae
ORION PAX <www.shamil.com91@gmail.com>
2025-12-23 21:48:34 +0300

    last two item is great done

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println(isStrong("1234567a"));
-        System.out.println(Arrays.toString(privatereqLowerCase("abAB!")));
+        System.out.println(Arrays.toString(charFreqLowerCase("abAB!")));
     }
 
     private static boolean isStrong(String password) {
