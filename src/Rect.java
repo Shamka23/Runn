2791c3a54dc49cafef632c5e373f1c45e44f65c9
ORION PAX <www.shamil.com91@gmail.com>
2025-12-24 19:28:59 +0300

    Classes is done

---
+++ src/Rect.java
@@ -0,0 +1,22 @@
+public class Rect {
+
+    private final int width;
+    private final int height;
+
+    public Rect(int width, int height) {
+        if (width <= 0 || height <= 0){
+            throw new IllegalArgumentException(
+                    "Длина или высота прямоугольника не может быть меньше или равняться нулю"
+            );
+        }
+        this.width = width;
+        this.height = height;
+    }
+    public int getArea(){
+        return width * height;
+    }
+
+    public int getPerimeter(){
+        return (width + height) * 2;
+    }
+}
