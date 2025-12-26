b99b91294aa2665b453ef84bb841b6ccfd2fc649
ORION PAX <www.shamil.com91@gmail.com>
2025-12-26 22:18:13 +0300

    вынес класс enum

---
+++ src/Level.java
@@ -0,0 +1,16 @@
+public enum Level {
+    EASY(1),
+    MEDIUM(2),
+    HARD(3),
+    EXPERT(5);
+
+    private final int points;
+
+    Level(int points) {
+        this.points = points;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,10 @@
-import java.util.logging.Level;
-
 public class Main {
 
     public static void main(String[] args) {
-        int p = Level.EXPERT.getPoints();
-        System.out.println(p);
-        Rect rectangle1 = new Rect(2, 7);
-        rectangle1.getPerimeter();
-        rectangle1.getArea();
+        int point = Level.EXPERT.getPoints();
+        System.out.println(point);
+        Rect rectangleFirst = new Rect(2, 7);
+        rectangleFirst.getPerimeter();
+        rectangleFirst.getArea();
     }
-
-    enum Level {
-        EASY(1),
-        MEDIUM(2),
-        HARD(3),
-        EXPERT(5);
-
-        private final int points;
-
-        Level(int points) {
-            this.points = points;
-        }
-
-        public int getPoints(){
-            return points;
-        }
-    }
-
-
 }
