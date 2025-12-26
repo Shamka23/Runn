87d73d252e87ec7c1e06cfb8a882802197730edf
ORION PAX <www.shamil.com91@gmail.com>
2025-12-26 19:30:46 +0300

    Исправил недочёты

--- src/cicles/Main.java
+++ src/cicles/Main.java
@@ -2,12 +2,12 @@ package cicles;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sumToN(4));
-        System.out.println(mulRow(120));
-        System.out.println(digitsCount(-12345));
+        System.out.println(getSumFromOneToNum(4));
+        System.out.println(getRowMultiplicationTable(120));
+        System.out.println(getDigitsCount(-12345));
     }
 
-    private static long sumToN(int number){
+    private static long getSumFromOneToNum(int number){
         int sum = 0;
         for (int i = 1; i <= number; i++) {
             sum += i;
@@ -15,7 +15,7 @@ public class Main {
         return sum;
     }
 
-    private static String mulRow(int number){
+    private static String getRowMultiplicationTable(int number){
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= 10 ; i++) {
             sb.append(number).append(" * ").append(i).append(" = ").append(number * i).append("\n");
@@ -23,7 +23,7 @@ public class Main {
         return sb.toString();
     }
 
-    private static int digitsCount(int number) {
+    private static int getDigitsCount(int number) {
         number = Math.abs(number);
         if(number == 0){
             return 1;
