c3ca3d4b496ec116dd6868bb1e5ea58b457feb9e
ORION PAX <www.shamil.com91@gmail.com>
2025-12-26 19:18:15 +0300

    Тяжело было понять как сделать побитовую операцию через математику, но я понял принцип

--- src/numbers/Main.java
+++ src/numbers/Main.java
@@ -3,22 +3,30 @@ package numbers;
 public class Main {
     public static void main(String[] args) {
         System.out.println(abs(-7));
-        System.out.println(fact(5));
+        System.out.println(isPowerOfTwo(32));
+        System.out.println(getFactorial(5));
         System.out.println(safeAvg(10, 8));
     }
 
-    private static int abs(int number){
+    private static int abs(int number) {
         return number < 0 ? -number : number;
     }
 
-    private static boolean isPowerOfTwo(int number){
-        if (number <= 0) return false;
-        return (number & (number - 1)) == 0;
+    private static boolean isPowerOfTwo(int number) {
+        if (number <= 0) {
+            return false;
+        }
+        while (number > 1) {
+            if (number % 2 != 0) {
+                return false;
+            }
+            number /= 2;
+        } return true;
     }
 
-    private static long fact(int number) {
+    private static long getFactorial(int number) {
         long factorial = 1;
-        if(number == 0){
+        if (number == 0) {
             return factorial;
         }
         for (int i = 1; i <= number; i++) {
@@ -27,7 +35,7 @@ public class Main {
         return factorial;
     }
 
-    private static int safeAvg(int firstNumber, int secondNumber){
-            return firstNumber + (secondNumber - firstNumber) / 2;
+    private static int safeAvg(int firstNumber, int secondNumber) {
+        return firstNumber + (secondNumber - firstNumber) / 2;
     }
 }
